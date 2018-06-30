7b87d663956135214df8700c4a5ffa8cd7793313
len0rd <tylerm15@gmail.com>
2018-06-29 21:04:49 -0400

    accepting advice from the internet and putting some thought into client-side api first

---
+++ core/src/main/java/net/lenords/yama/CrawlBuilder.java
@@ -0,0 +1,13 @@
+package net.lenords.yama;
+
+
+/**
+ *
+ * Yama public API
+ *
+ * @author len0rd
+ * @since 2018-06-29
+ */
+public interface CrawlBuilder {
+
+}
---
+++ core/src/main/java/net/lenords/yama/CrawlManager.java
@@ -0,0 +1,31 @@
+package net.lenords.yama;
+
+import net.lenords.yama.internal.crawler.Crawler;
+
+import java.util.Optional;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Yama public API
+ *
+ * @author len0rd
+ * @since 2018-06-29
+ */
+public interface CrawlManager {
+
+    void importCrawler(String filename);
+
+    void importCrawler(Crawler crawler);
+
+    void runCrawler(String crawlerName);
+
+    void runCrawler(Crawler crawler);
+
+    Properties globalProperties();
+
+    Optional<Crawler> crawler(String crawlerName);
+
+    Set<Crawler> activeCrawlers();
+
+}
